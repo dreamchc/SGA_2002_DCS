be56935e31cbcec12672c23ca3e83e5231b463bd
dreamchc <dreamchc@naver.com>
2020-08-27 11:31:06 +0900

    GameMode Setting_Cpp

---
+++ Source/U2002_05_DCS/GameModes/CGameMode_Combat.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CGameMode_Combat.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class U2002_05_DCS_API ACGameMode_Combat : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
